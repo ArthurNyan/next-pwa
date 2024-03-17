9203675947e8377bf8e9e5afae0c1cfb192718c6
Артур Нахатакян <artoim333@gmail.com>
2024-03-17 12:14:29 +0300

    feat: make pwa

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import withPWAInit from "next-pwa";
 
-export default nextConfig;
+const withPWA = withPWAInit({
+  dest: "public",
+});
+
+export default withPWA({});
